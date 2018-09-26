33deae30e398a09ec90f598248014f018d187c07
hunter199129 <hunter199129@gmail.com>
2018-09-26 08:31:51 +0000

    Upgrade select2 3.4.5 to 4.0.5

---
+++ assets/javascripts/select_to_select2.js
@@ -0,0 +1,66 @@
+/*
+ Select To Select2 Plugin Main.
+ */
+
+(function($){
+
+    // For Page Load
+    replaceAllSelect2();
+
+    // For Ajax
+    $(document).ajaxComplete(function(event){
+        replaceAllSelect2();
+    });
+
+    // For GET,POST Request
+    $(window).load(function() {
+        replaceAllSelect2();
+    });
+
+    // for all elements
+    // click event only for toggle to multiple select
+    $(document).click(function(event){
+        if (event.target.className === 'toggle-multiselect') replaceAllSelect2();
+    });
+
+    // for all elements
+    // prevent bug from choosing searched result
+    $(document).change(function(event){
+        setTimeout(function() {
+            replaceAllSelect2();
+        }, 100);
+    });
+
+}(jQuery));
+
+function replaceAllSelect2(){
+
+    var elements = document.getElementsByTagName("select");
+
+    for (i = 0; i < elements.length; i++) {
+
+        // For not woroking 「width:resolve」
+        if(elements[i].id == 'year'
+        || elements[i].id == 'month'
+        || elements[i].id == 'columns'
+        || elements[i].id == 'settings_issuequery_query_id'
+        || elements[i].id == 'block-select'){
+
+            $("#" + elements[i].id).select2({
+                width:"175px",
+                placeholder: "",
+                allowClear: true
+            });
+        }
+        else {
+            // For All Pages
+            $("#" + elements[i].id).select2({
+                width:"resolve",
+                placeholder: "",
+                allowClear: true
+            });
+        }
+
+    }
+
+}
